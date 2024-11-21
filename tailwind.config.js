999db717de568a25ba6408889b2977fa090193dc
minhnguyen9702 <minhnguyen9702@gmail.com>
2024-11-21 15:47:46 +0700

    configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // Include all React and TypeScript files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
