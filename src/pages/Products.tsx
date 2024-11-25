1a1f6b17b0497d0b27ef6e73031bf587eaceb351
minhnguyen9702 <minhnguyen9702@gmail.com>
2024-11-25 16:41:15 +0700

    add sorting items

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -13,6 +13,8 @@ type Product = {
 
 const Products = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [sortedProducts, setSortedProducts] = useState<Product[]>([]);
+  const [sortOrder, setSortOrder] = useState("default");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -34,6 +36,34 @@ const Products = () => {
         setLoading(false);
       });
   }, []);
+
+  useEffect(() => {
+    if (loading) return;
+
+    let sorted: Product[];
+    if (sortOrder === "price-asc") {
+      sorted = [...products].sort(
+        (a, b) => parseFloat(a.price) - parseFloat(b.price)
+      );
+    } else if (sortOrder === "price-desc") {
+      sorted = [...products].sort(
+        (a, b) => parseFloat(b.price) - parseFloat(a.price)
+      );
+    } else if (sortOrder === "alphabetical-asc") {
+      sorted = [...products].sort((a, b) => a.title.localeCompare(b.title));
+    } else if (sortOrder === "alphabetical-desc") {
+      sorted = [...products].sort((a, b) => b.title.localeCompare(a.title));
+    } else if (sortOrder === "default") {
+      sorted = [...products].sort((a, b) => a.id - b.id);
+    }
+
+    setSortedProducts(sorted);
+  }, [products, sortOrder, loading]);
+
+  const handleSortChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const order = e.target.value;
+    setSortOrder(order);
+  };
 
   if (loading) {
     return (
@@ -57,9 +87,20 @@ const Products = () => {
 
   return (
     <Wrapper>
+      <select
+        onChange={handleSortChange}
+        value={sortOrder}
+        className="p-2 border rounded-md shadow-md bg-white text-gray-700 focus:outline-none focus:ring-2 focus:ring-sky-400"
+      >
+        <option value="default">Default</option>
+        <option value="price-asc">Price: Low to High</option>
+        <option value="price-desc">Price: High to Low</option>
+        <option value="alphabetical-asc">Name: A-Z</option>
+        <option value="alphabetical-desc">Name: Z-A</option>
+      </select>
       <div className="grow flex flex-wrap justify-center">
-        {products.map((product) => (
-          <ProductCard product={product}/>
+        {sortedProducts.map((product) => (
+          <ProductCard product={product} />
         ))}
       </div>
     </Wrapper>
